38809403c1a55a36e9c5e164bab00c82c5c038b8
David Simpson <31688862+ds-04@users.noreply.github.com>
2022-04-13 16:54:41 +0100

    Update gluster_eval_gfids_on_brick.sh

--- gluster_eval_gfids_on_brick.sh
+++ gluster_eval_gfids_on_brick.sh
@@ -12,6 +12,11 @@
 # 4. Print and store each path in log
 # 5. You can then (elsewhere) perform ls on the mounted volume (client) to trigger healing
 
+echo "SCRIPT BEING FIXED"
+echo "AWAIT UPDATE ON Github"
+exit 1
+
+
 TMP_LOG=`mktemp`
 
 if [[ "$#" -lt "2" || "$#" -gt "3" ]]
