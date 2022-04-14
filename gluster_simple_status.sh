abef6276589599536facbea1604bf4b668afcee6
David Simpson <31688862+ds-04@users.noreply.github.com>
2022-04-14 12:59:56 +0100

    Update gluster_simple_status.sh
    tidy

--- gluster_simple_status.sh
+++ gluster_simple_status.sh
@@ -8,6 +8,6 @@ echo $HOSTNAME
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo "$dt"
 echo -e '\x1b[0m'
-echo
+echo ""
 gluster volume status all | sed -e 's/\sY\s/\x1b[0;32m&\x1b[0m/' -e 's/\sN\s/\x1b[0;33m&\x1b[0m/'
 gluster peer status | sed -e 's/Connected/\x1b[0;32m&\x1b[0m/'
